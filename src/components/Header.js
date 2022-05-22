43d96a6759c85a6b9b3151e2f022c85ed4476396
Irving Vasquez <ivasquez@hypernovalabs.com>
2022-05-21 20:46:39 -0500

    quitando boostrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,112 +1,72 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import logo from "../assets/img/logo.jpg";
 export default function () {
   return (
-    <Navbar
-      bg="light"
-      expand="lg"
-      className="collapse navbar-collapse justify-content-between"
-    >
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    // <div
-    //   className="collapse navbar-collapse justify-content-between"
-    //   id="navbarSupportedContent"
-    // >
-    //   <ul className="navbar-nav d-flex justify-content-between">
-    //     <li className="nav-item only-desktop">
-    //       <a className="nav-link" id="side-nav-open" href="#">
-    //         <span className="lnr lnr-menu"></span>
-    //       </a>
-    //     </li>
-    //     <div className="d-flex flex-lg-row flex-column">
-    //       <li className="nav-item active">
-    //         <a className="nav-link" href={logo}>
-    //           Home <span className="sr-only">(current)</span>
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="about.html">
-    //           About
-    //         </a>
-    //       </li>
+    
+    <nav id="navbar-header" class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand navbar-brand-center d-flex align-items-center p-0 only-mobile" href="/">
+   
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="lnr lnr-menu"></span>
+        </button>
 
-    //       <li className="nav-item dropdown">
-    //         <a
-    //           className="nav-link dropdown-toggle"
-    //           href="#"
-    //           id="navbarDropdown"
-    //           role="button"
-    //           data-toggle="dropdown"
-    //           aria-haspopup="true"
-    //           aria-expanded="false"
-    //         >
-    //           Special Dishes
-    //         </a>
-    //         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-    //           <a className="dropdown-item" href="special-dishes.html">
-    //             Beef Steak Sauce
-    //           </a>
-    //           <a className="dropdown-item" href="special-dishes.html">
-    //             Salmon Zucchini
-    //           </a>
-    //         </div>
-    //       </li>
-    //     </div>
-    //   </ul>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav d-flex justify-content-between">
+                <li class="nav-item only-desktop">
+                    <a class="nav-link" id="side-nav-open" href="#">
+                        <span class="lnr lnr-menu"></span>
+                    </a>
+                </li>
+                <div class="d-flex flex-lg-row flex-column">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">About</a>
+                    </li>
 
-    //   <a
-    //     className="navbar-brand navbar-brand-center d-flex align-items-center only-desktop"
-    //     href="#"
-    //   >
-    //     <img src={logo} alt="" />
-    //   </a>
-    //   <ul className="navbar-nav d-flex justify-content-between">
-    //     <div className="d-flex flex-lg-row flex-column">
-    //       <li className="nav-item active">
-    //         <a className="nav-link" href="menu.html">
-    //           Menu
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="team.html">
-    //           Team
-    //         </a>
-    //       </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Special Dishes
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="special-dishes.html">Beef Steak Sauce</a>
+                            <a class="dropdown-item" href="special-dishes.html">Salmon Zucchini</a>
+                        </div>
+                    </li>
+                </div>
+            </ul>
 
-    //       <li className="nav-item dropdown">
-    //         <a className="nav-link" href="reservation.html">
-    //           Reservation
-    //         </a>
-    //       </li>
-    //     </div>
-    //     <li className="nav-item">
-    //       <a id="side-search-open" className="nav-link" href="#">
-    //         <span className="lnr lnr-magnifier"></span>
-    //       </a>
-    //     </li>
-    //   </ul>
-    // </div>
+            <a class="navbar-brand navbar-brand-center d-flex align-items-center only-desktop" href="#">
+              
+            </a>
+            <ul class="navbar-nav d-flex justify-content-between">
+                <div class="d-flex flex-lg-row flex-column">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="menu.html">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="team.html">Team</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="reservation.html">Reservation</a>
+                    </li>
+                </div>
+                <li class="nav-item">
+                    <a id="side-search-open" class="nav-link" href="#">
+                        <span class="lnr lnr-magnifier"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav> 
+   
   );
 }
